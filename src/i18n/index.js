2495223e05eb9143627886199b51ef141db9d9ff
Murhussain <husseinmurashi@yahoo.com>
2023-05-08 09:45:01 +0200

    Setting up .env for default language

---
+++ src/i18n/index.js
@@ -0,0 +1,7 @@
+import { createI18n } from "vue-i18n";
+
+export default createI18n({
+  locale: import.meta.env.VITE_DEFAULT_LOCALE, // <--- 1
+  fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE, // <--- 2
+  legacy: false, // <--- 3
+})
