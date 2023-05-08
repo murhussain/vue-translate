72d56384d48b7cf2eaa9664afa3a950c2fd9773a
Murhussain <husseinmurashi@yahoo.com>
2023-05-08 14:05:05 +0200

    Done with implementing language switcher

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,10 +4,17 @@
       <li>
         <RouterLink to="/">{{ $t("nav.home")}}</RouterLink>
       </li>
-      
       <li>
         <RouterLink to="/about">{{ $t("nav.about")}}</RouterLink>
       </li>
     </ul>
   </nav>
+  <LanguageSwitcher></LanguageSwitcher>
 </template>
+
+<script>
+  import LanguageSwitcher from "@/components/LanguageSwitcher.vue"
+  export default {
+    components: { LanguageSwitcher }
+  }
+</script>
---
+++ src/i18n/translation.js
@@ -0,0 +1,16 @@
+import i18n from "@/i18n"
+
+const Trans = {
+  get supportedLocales() {
+    return import.meta.env.VITE_SUPPORTED_LOCALES.split(",")
+  },
+  set currentLocale(newLocale) { // <--- 2
+    i18n.global.locale.value = newLocale
+  },
+  async switchLanguage(newLocale) { // <--- 3
+    Trans.currentLocale = newLocale
+    document.querySelector("html").setAttribute("lang", newLocale)
+  },
+}
+
+export default Trans
