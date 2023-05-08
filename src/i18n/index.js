394dd18064947351810d487011be9f9e801ee7c3
Murhussain <husseinmurashi@yahoo.com>
2023-05-08 11:50:40 +0200

    Dealing with pluralization strings

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,4 +1,5 @@
 import { createI18n } from "vue-i18n";
+import pluralRules from "./rules/pluralization" // <--- add this 
 
 const messages = {
   en: {
@@ -8,7 +9,8 @@ const messages = {
     },
     home: {
       header: "Welcome to the Vue 3 I18n tutorial!",
-      created_by: "This tutorial was brought to you by {company}."
+      created_by: "This tutorial was brought to you by {company}.",
+      num_visits: "This page hasn't been visited :( | This page has been visited {n} time | This page has been visited {n} times"
     },
     about: {
       header: "About us"
@@ -22,7 +24,8 @@ const messages = {
     },
     home: {
       header: "Добро пожаловать в руководство Vue 3 I18n!",
-      created_by: "Это руководство создано для вас компанией {company}."
+      created_by: "Это руководство создано для вас компанией {company}.",
+      num_visits: "Страницу не посещали :( | Страницу посетили {n} раз | Страницу посетили {n} раза | Страницу посетили {n} раз"
     },
     about: {
       header: "О нас"
@@ -35,5 +38,6 @@ export default createI18n({
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE, // <--- 2
   legacy: false, // <--- 3
   globalInjection: true,
-  messages
+  messages,
+  pluralRules // <--- add this
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,6 @@
   <main>
     <h1>{{ $t("home.header") }}</h1>
     <p>{{ $t("home.created_by", {company: "Lokalise"}) }}</p>
-    <p>This page has been visited 30 times.</p>
+    <p>{{ $t("home.num_visits", 0) }}</p>
   </main>
 </template>
